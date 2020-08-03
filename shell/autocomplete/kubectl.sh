eb477431bcde9df0ffda449f3ab3d89f3a6bafd5
L Philbrook <stuff@whatafool.com>
2020-08-03 13:31:08 -0500

    ADD autocomplete for kubectl

--- shell/autocomplete/kubectl.sh
+++ shell/autocomplete/kubectl.sh
@@ -2,4 +2,8 @@ if [[ "$(uname)" == "Darwin" ]]; then
   if [ -f `brew --prefix`/etc/bash_completion.d/kubectl ]; then
     kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
   fi
+else
+  if [[  -f "$HOME/.asdf/shims/kubectl" ]]; then
+    source <($HOME/.asdf/shims/kubectl completion bash)
+  fi
 fi
